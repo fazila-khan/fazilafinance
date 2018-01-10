44e25a7f3a242f23bd0f4c52c10618f77bd90de1
Fazila Ashraf <fazila.ashraf@lighting.com>
2018-01-10 22:51:32 +0530

    FD data read from user and stored in a json file

--- venv/fd.py
+++ venv/fd.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from datetime import timedelta
+from pathlib import Path
+import json
+import os.path
 
 fd_list=[]
-
 
 def get_fd_data():
     fd={}
     fd['bank']= input("Enter the Bank name: ")
-    fd['start_date'] = input("Enter the FD start date: ")
+    fd['start_date'] = input("Enter the FD start date (in format dd-mm-yy) : ")
     fd['principal']= input("Enter the principal amount: ")
     fd['interest']= input("Enter the interest rate: ")
     fd['duration']=input("Enter the duration of investment (in days): ")
@@ -19,16 +21,16 @@ def get_fd_data():
 
 def calc_maturity_date(fd):
     t_maturity_date= datetime.strptime(fd['start_date'],'%d-%m-%y') + timedelta(days=int(fd['duration']))
-    return t_maturity_date
+    return t_maturity_date.isoformat()
 
 
 def calc_maturity_amount(fd):
     t_interest= int(fd['interest'])/100
-    print("Interest in formula : %.2f " %(t_interest))
+    #print("Interest in formula : %.2f " %(t_interest))
     t_duration= int(fd['duration'])/365
-    print("Time in formulat is %.2f" %(t_duration))
+    #print("Time in formulat is %.2f" %(t_duration))
     t_maturity_amount= int(fd['principal']) * (1 + (t_interest * t_duration))
-    print("The maturity amount in the function is %.2f" %(t_maturity_amount))
+    #print("The maturity amount in the function is %.2f" %(t_maturity_amount))
     return t_maturity_amount
 
 
@@ -44,8 +46,34 @@ def print_fd_list():
         print("Goal : " + fd['goal'])
         print("************************************************************")
 
+
+def save_file():
+    try:
+        file_path=Path("FD_data.json")
+        if file_path.exists():
+            print(file_path.exists())
+            f = open("FD_data.json", "ab")
+            f.seek(-1,os.SEEK_END)
+            f.truncate()
+            for fd in fd_list:
+                f.write(b',')
+                f.write(json.dumps(fd).encode('utf-8'))
+            f.write(b']')
+        else:
+            f = open("FD_data.json","ab")
+            f.write(b'[')
+            for fd in fd_list:
+                f.write(json.dumps(fd).encode('utf-8'))
+            f.write(b']')
+            f.close()
+    except FileNotFoundError:
+        print("Error saving data to file")
+
+
 choice = input("Enter 1 for new FD addition: ")
 if choice == "1":
     get_fd_data()
-    print_fd_list()
+    #print_fd_list()
+    save_file()
+    print ("Thank you!")
 
